e44e93ef7c9917176db2ec4a583f08d76d10376d
David Mejia <franklin.mejia@epn.edu.ec>
2019-02-01 11:31:35 -0500

    Interfaces listas.

---
+++ app/src/main/java/com/example/examen/AtributosHijoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.examen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AtributosHijoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_atributos_hijo)
+    }
+}
--- app/src/main/java/com/example/examen/MainActivity.kt
+++ app/src/main/java/com/example/examen/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.examen
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCrearTabla.setOnClickListener { irACrearTablaPadre() }
+        btnListarListaPadre.setOnClickListener { irAListarTabla() }
+    }
+
+    fun irACrearTablaPadre(){
+
+        val intent = Intent(this, CrearTablaActivity::class.java)
+        startActivity(intent)
+    }
+    fun irAListarTabla(){
+        val intent = Intent(this, ListarTablaPadreActivity::class.java)
+        startActivity(intent)
     }
 }
