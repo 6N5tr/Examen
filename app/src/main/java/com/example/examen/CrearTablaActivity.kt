e20d953ee0992214363c51ca3a3472ac8d6e604f
David Mejia <franklin.mejia@epn.edu.ec>
2019-02-04 20:48:39 -0500

    Cuadros De Dialogo con ANKO

--- app/src/main/java/com/example/examen/CrearTablaActivity.kt
+++ app/src/main/java/com/example/examen/CrearTablaActivity.kt
@@ -13,10 +13,10 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.ValueEventListener
-
-
-
-
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.yesButton
 
 
 class CrearTablaActivity : AppCompatActivity() {
@@ -47,26 +47,33 @@ class CrearTablaActivity : AppCompatActivity() {
     fun crearTabla() {
 
 
-        
-        etNombreEquipo = findViewById(R.id.etNombreEquipo) as EditText
-        etNombreLiga = findViewById(R.id.etNombreLiga) as EditText
-        etFechaCreacion = findViewById(R.id.etFechaCreacion) as EditText
-        etCopasInter = findViewById(R.id.etCopasInter) as EditText
-        etCampeonActual = findViewById(R.id.etCampeonActual) as EditText
+        alert("Desea guardar los datos?") {
+
+            yesButton { etNombreEquipo = findViewById(R.id.etNombreEquipo) as EditText
+                etNombreLiga = findViewById(R.id.etNombreLiga) as EditText
+                etFechaCreacion = findViewById(R.id.etFechaCreacion) as EditText
+                etCopasInter = findViewById(R.id.etCopasInter) as EditText
+                etCampeonActual = findViewById(R.id.etCampeonActual) as EditText
 
 
-        val database = FirebaseDatabase.getInstance()
-        val ref = database.getReference("Equipo")
+                val database = FirebaseDatabase.getInstance()
+                val ref = database.getReference("Equipo")
 
-        val equipoid=ref.push().key
-        val equipo = Equipo(equipoid.toString(),etNombreEquipo?.text.toString(),etNombreLiga?.text.toString(),etFechaCreacion?.text.toString(),
-            etCopasInter?.text.toString(),etCampeonActual?.text.toString())
+                val equipoid=ref.push().key
+                val equipo = Equipo(equipoid.toString(),etNombreEquipo?.text.toString(),etNombreLiga?.text.toString(),etFechaCreacion?.text.toString(),
+                    etCopasInter?.text.toString(),etCampeonActual?.text.toString())
 
 
-        ref.child(equipoid!!).setValue(equipo).addOnCompleteListener{
+                ref.child(equipoid!!).setValue(equipo).addOnCompleteListener{
 
-                Toast.makeText(applicationContext,"Todo Bien",Toast.LENGTH_SHORT).show()
-        }
+                    Toast.makeText(applicationContext,"Agregado Exitosamente",Toast.LENGTH_SHORT).show()
+
+
+                }}
+            noButton { }
+        }.show()
+
+
 
     }
 }
